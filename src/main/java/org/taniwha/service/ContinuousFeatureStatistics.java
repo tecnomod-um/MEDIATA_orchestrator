8d67d2da45169dce10c402b8cb870a20ea6cf6c3
danibanezRepos <danibanez.info@gmail.com>
2024-02-29 15:46:49 +0100

    Improved on stat fetching

---
+++ src/main/java/org/taniwha/service/ContinuousFeatureStatistics.java
@@ -0,0 +1,32 @@
+package org.taniwha.service;
+
+import org.taniwha.service.FeatureStatistics;
+import java.util.List;
+import java.util.Map;
+
+// ContinuousFeatureStatistics class
+public class ContinuousFeatureStatistics extends FeatureStatistics {
+    private int cardinality;
+    private Map<String, Object> statistics;
+    private List<Double> outliers;
+
+    public ContinuousFeatureStatistics(String featureName, long count, double percentMissing, long missingValuesCount, int cardinality, Map<String, Object> statistics, List<Double> outliers) {
+        super(featureName, count, percentMissing, missingValuesCount);
+        this.cardinality = cardinality;
+        this.statistics = statistics;
+        this.outliers = outliers;
+    }
+
+    @Override
+    public Map<String, Object> getTypeStatistics() {
+        return statistics;
+    }
+
+    public int getCardinality() {
+        return cardinality;
+    }
+
+    public List<Double> getOutliers() {
+        return outliers;
+    }
+}
