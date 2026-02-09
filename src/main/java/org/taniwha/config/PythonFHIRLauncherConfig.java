ac9f9c7f6848519bd830cf8eca38079e2be5016d
danibanezRepos <danibanez.info@gmail.com>
2026-02-09 18:03:54 +0100

    Added functionallity toggling. Backend can now optionally launch without submodules

--- src/main/java/org/taniwha/config/PythonFHIRLauncherConfig.java
+++ src/main/java/org/taniwha/config/PythonFHIRLauncherConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -20,6 +21,7 @@ import static org.taniwha.util.PythonLauncherUtil.*;
 
 @Profile("!docker")
 @Configuration
+@ConditionalOnProperty(name = "fhir.launcher.enabled", havingValue = "true", matchIfMissing = true)
 public class PythonFHIRLauncherConfig {
     private static final Logger logger = LoggerFactory.getLogger(PythonFHIRLauncherConfig.class);
 
